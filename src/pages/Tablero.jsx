93c6fcd3e68ca7cc4293cbc8a2ff787297621e5c
Omega <elbackenddev22@gmail.com>
2025-06-01 16:01:57 -0600

    Comenzanco panel de backlog y añadiendo función de botón para abrir el panel de desarrollo de backlog

--- src/pages/Tablero.jsx
+++ src/pages/Tablero.jsx
@@ -1,8 +1,35 @@
+import { useState } from 'react';
+import '../styles/Tablero.css';
+
 function Tablero() {
+
+    const [open, setOpen] = useState(false);
+
+    function toggleBoard() {
+        setOpen(!open);
+    }
+
     return (
         <div>
-            <h2>Este es tu tablero</h2>
-            <p>Aquí gestionarás tu backlog.</p>
+            <div className="tableroContainer">
+                <h1 className='titleTablero'>Este es tu tablero</h1>
+            <p className='subtitleTablero'>Aquí gestionarás tu backlog.</p>
+            </div>
+
+            <section className='sectionResources'>
+                <button className='btnCreateBoard' onClick={toggleBoard}>
+                    { open ? 'Cerrar Tablero' : 'Crear Tablero' }
+                </button>
+            </section>
+
+            <section className='backlogDevelopment'>
+                {open && (
+                    <div className='panelCentro'>
+                            <h2>Panel de Creación del Backlog</h2>
+        <p>Aquí puedes empezar a añadir elementos.</p>
+                    </div>
+                )}
+            </section>
         </div>
     );
 }
